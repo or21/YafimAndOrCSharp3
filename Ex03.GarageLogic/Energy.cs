e8acc468dec2f4dd0ada0491b4dfd166617bb34a
or21 <orbrand21@gmail.com>
2015-05-26 10:50:21 +0300

    fffffff

---
+++ Ex03.GarageLogic/Energy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    public class Energy
+    {
+        private float currentAmount;
+        private readonly float r_MaxAmount;
+
+        public Energy(float i_CurrentAmount, float i_MaxAmount)
+        {
+            this.currentAmount = i_CurrentAmount;
+            this.r_MaxAmount = i_MaxAmount;
+        }
+
+        public void Load(float i_EnergyToAdd)
+        {
+            float sumOfAmounts = currentAmount + i_EnergyToAdd;
+            bool isOverMax = sumOfAmounts > r_MaxAmount;
+            if (isOverMax)
+            {
+                // TODO: Throw exception ;
+            }
+            else
+            {
+                this.currentAmount = sumOfAmounts;
+            }
+        }
+
+        public float CurrAmount
+        {
+            get { return this.currentAmount; }
+            set { this.currentAmount = value; }
+        }
+    }
+}
