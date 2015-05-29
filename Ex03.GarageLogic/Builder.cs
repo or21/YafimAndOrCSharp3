d7c5f8e3a4c2b59126925dd226a1e01bbe6ab310
yafim <ifim.vo@gmail.com>
2015-05-29 21:20:51 +0300

    builder calss

---
+++ Ex03.GarageLogic/Builder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public class Builder
+    {
+        // General fields for all viehicles
+        private List<object> m_uniqueProperties;
+        private eVehicle m_vehicle;
+        private Vehicle vehicle;
+
+        public void createVehicle(eVehicle i_Vehicle, string i_VehicleManufacturer, string i_Id, bool i_IsElectric, float i_CurrentAmountOfPowerSource, float i_CurrentAmountOfAir, string i_WheelManufacturer, List<object> i_UniqueProperties)
+        {
+            // Build specific vehecile
+            switch (i_Vehicle)
+            {
+                case eVehicle.CAR:
+                    eColor color = (eColor) i_UniqueProperties[0];
+                    int numberOfDoors = (int) i_UniqueProperties[1];
+                    vehicle = new Car(i_VehicleManufacturer, i_Id, i_IsElectric, i_CurrentAmountOfPowerSource, i_CurrentAmountOfAir, i_WheelManufacturer, color, numberOfDoors);
+                    break;
+                case eVehicle.MOTOR:
+                    eLicense license = (eLicense)i_UniqueProperties[0];
+                    int egine = (int) i_UniqueProperties[1];
+                    vehicle = new Motor(i_VehicleManufacturer, i_Id, i_IsElectric, i_CurrentAmountOfPowerSource, i_CurrentAmountOfAir, i_WheelManufacturer, license, egine);
+                    break;
+                case eVehicle.TRUK:
+                    bool isCarryingDangerous = (bool)i_UniqueProperties[0];
+                    int currentCarryWeight = (int) i_UniqueProperties[1];
+                    vehicle = new Truk(i_VehicleManufacturer, i_Id, i_IsElectric, i_CurrentAmountOfPowerSource, i_CurrentAmountOfAir, i_WheelManufacturer, isCarryingDangerous, currentCarryWeight);
+                    break;
+
+                // Add more vehicles here...
+            }
+        }
+
+    }
+
+    public enum eVehicle
+    {
+        CAR,
+        MOTOR,
+        TRUK
+        // add more supported vehicles here
+    }
+}
--- Ex03.GarageLogic/Car.cs
+++ Ex03.GarageLogic/Car.cs
@@ -13,22 +13,25 @@
         private static readonly float m_fuel = (float)35;
         private static readonly Fuel.eFuelType m_fuelType = Fuel.eFuelType.Octan96;
 
-        public Car(string iVehicleManufacturer, string iId, bool i_IsElectric, float i_CurrentAmountOfPowerSource, float i_CurrentAmountOfAir, string i_WheelManufacturer, eColor i_Color, int i_NumberOfDoors)
-            : base(iVehicleManufacturer, iId, i_CurrentAmountOfPowerSource, m_numberOfWheels, i_IsElectric, (i_IsElectric) ? m_electricMaxAir : m_fuelMaxAir, i_WheelManufacturer, (i_IsElectric) ? m_energy : m_fuel, m_fuelType)
+        public Car(string i_VehicleManufacturer, string i_Id, bool i_IsElectric, float i_CurrentAmountOfPowerSource, float i_CurrentAmountOfAir, string i_WheelManufacturer, eColor i_Color, int i_NumberOfDoors)
+         : base(i_VehicleManufacturer, i_Id, i_CurrentAmountOfPowerSource, m_numberOfWheels, i_IsElectric, (i_IsElectric) ? m_electricMaxAir : m_fuelMaxAir, i_WheelManufacturer, (i_IsElectric) ? m_energy : m_fuel, m_fuelType)
         {
             // Set additional unique properties
             this.m_color = i_Color;
             this.m_numberOfDoors = i_NumberOfDoors;
         }
 
+
         public int Doors
         {
             get { return this.m_numberOfDoors; }
+            set { m_numberOfDoors = value; }
         }
 
         public eColor Color
         {
             get { return this.m_color; }
+            set { m_color = value; }
         }
     }
 
